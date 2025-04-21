ff4ed88e1caad22381ca930a274407c6eedb36a9
kwangminleee <nuewaz@naver.com>
2025-04-21 21:08:51 +0900

    EnemyPhase
    아직 미완성입니다..

--- Program.cs
+++ Program.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
 
-namespace MyApp
+namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void Battle(Player player)
         {
-            Console.WriteLine("Hello World!");
+            int monsterIndex = 0;
+            int totaldamage = 0;
+
+
+            while (true)
+            {
+                if (monsterIndex >= monsters.Length)
+                {
+                    //플레이어턴
+                    continue;
+                }
+
+                var monster = monsters[monsterIndex];
+                monsterIndex++;
+
+                if (monster.IsDead)
+                {
+                    Console.WriteLine($"{monster.Name}은(는) 죽어서 공격할 수 없습니다.");
+                    continue;
+                }
+
+
+                Console.WriteLine("Battle!!");
+
+
+                Console.WriteLine($"\n[{monster.Name}의 차례]");
+                Console.WriteLine(">> 다음을 누르세요...");
+                Console.ReadLine();
+
+              
+
+                Console.WriteLine($"{monster.Name}의 공격! [데미지: {monster.Atk}]");
+                
+
+
+                Console.WriteLine("\n영웅이 아파합니다.\n");
+
+                Console.WriteLine("\n[내 정보]");
+                Console.WriteLine($"Lv.{player.Level} {player.Name} ({player.Class})");
+                Console.WriteLine($"HP {player.Hp}/100 (-{monster.Atk})");
+
+                if (player.Hp <= 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n[플레이어가 사망했습니다...]");
+                    Console.ResetColor();
+
+                    Console.WriteLine("0. 메뉴로 돌아가기");
+                    string defeat = Console.ReadLine();
+                    if (defeat == "0")
+                    {
+
+                    }
+                    else
+                    {
+
+                    }
+                    return;
+                }
+
+                Console.WriteLine("\n0.다음");
+            }
         }
     }
 }
