2030e2374ddf37b461fc8ce4473e6bea72d5651a
박원희 <spilfive3@gmail.com>
2025-04-25 11:02:33 +0900

    [Fix] NullException Resolve
    GameManager를 통해 참조 문제를 해결했습니다

--- IPG/IPG/Script/BattleManager.cs
+++ IPG/IPG/Script/BattleManager.cs
@@ -1,65 +1,52 @@
-﻿using System.Reflection.Emit;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using System.Transactions;
 using System.Xml.Linq;
+using 연습장.Script;
+
 namespace IPG
 {
     internal class BattleManager
     {
-        static PlayerController player;
-        static DungeonLobbyController dungeonLobby;
-
-        public BattleManager(PlayerController injectedPlayer, Battlecontroller injectedController)
+        public static bool StartBattleAndCheckVictory()
         {
-            player = injectedPlayer;
-            controller = injectedController;
-        }
-        static List<MonsterController> monsters;
-        static VillageController village;
-        static Battlecontroller controller;
+            PlayerAttackPhase();
 
-
-        public static void SetMonsters(List<MonsterController> newMonsters)
-        {
-            monsters = newMonsters;
+            bool allDead = true;
+            foreach (var monster in GameManager.ListMonsters)
+            {
+                if (!monster.IsDead)
+                {
+                    allDead = false;
+                    break;
+                }
+            }
+            return allDead;
         }
 
-        public void SetVillage(VillageController v)
-        {
-            village = v;
-        }
-
-        public void SetDungeonLobby(DungeonLobbyController d)
-        {
-            dungeonLobby = d;
-        }
-
-        // 공격 턴 UI
-        static public void PlayerAttackPhase()
+        public static void PlayerAttackPhase()
         {
             int input = -1;
 
             while (input != 0)
             {
-                
+
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("My turn");
                 Console.ResetColor();
                 Console.WriteLine();
 
-                for (int i = 0; i < monsters.Count; i++)
-                {
-                    monsters[i].ShowMonsterInfo(i + 1);
-                }
+                GameManager.MonsterController.ShowMonsterInfo();
 
-                player.ShowPlayerInfo();
+                GameManager.PlayerController.ShowPlayerInfo();
 
                 Console.WriteLine("\n0. 도망치기");
                 Console.WriteLine("\n대상을 선택해주세요.");
                 Console.Write(">> ");
 
                 bool isValid = int.TryParse(Console.ReadLine(), out input);
-                if (!isValid || input < 0 || input > monsters.Count)
+                if (!isValid || input < 0 || input > GameManager.ListMonsters.Count)
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("잘못된 입력입니다.");
@@ -70,13 +57,13 @@ namespace IPG
                 if (input == 0)
                 {
                     Console.WriteLine("전투에서 도망쳤습니다.");
-                    controller.Battlestart();
+                    GameManager.BattleController.Battlestart();
                     break;
                 }
 
                 // 모든 몬스터가 죽었는지 확인
                 bool allDead = true;
-                foreach (var monster in monsters)
+                foreach (var monster in GameManager.ListMonsters)
                 {
                     if (!monster.IsDead)
                     {
@@ -90,7 +77,7 @@ namespace IPG
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("모든 몬스터를 처치했습니다! 전투 종료!");
                     Console.ResetColor();
-                    controller.Battlevictory();
+                    GameManager.BattleController.Battlevictory();
                     break;
                 }
 
@@ -98,13 +85,13 @@ namespace IPG
                 AttackMonster(input);
                 Console.WriteLine("계속하려면 아무 키나 누르세요.");
                 Console.ReadKey(true);
-                
-            } 
+
+            }
         }
 
         static void AttackMonster(int input)
         {
-            MonsterController targetMonster = monsters[input - 1];
+            MonsterController targetMonster = GameManager.ListMonsters[input - 1];
 
             if (targetMonster.IsDead)
             {
@@ -114,8 +101,8 @@ namespace IPG
                 return;
             }
 
-            int minDamage = (int)Math.Ceiling(player.baseAtk * 0.9);
-            int maxDamage = (int)Math.Ceiling(player.baseAtk * 1.1);
+            int minDamage = (int)Math.Ceiling(GameManager.PlayerController.baseAtk * 0.9);
+            int maxDamage = (int)Math.Ceiling(GameManager.PlayerController.baseAtk * 1.1);
 
             Random random = new Random();
             int damage = random.Next(minDamage, maxDamage + 1);
@@ -142,10 +129,34 @@ namespace IPG
 
             Console.WriteLine("");
 
-            for (int i = 0; i < monsters.Count; i++)
-            {
-                monsters[i].ShowMonsterInfo(i + 1);
-            }
+            GameManager.MonsterController.ShowMonsterInfo();
+
+             // 모든 몬스터가 죽었는지 확인
+                bool allDead = true;
+                foreach (var monster in GameManager.ListMonsters)
+                {
+                    if (!monster.IsDead)
+                    {
+                        allDead = false;
+                        break;
+                    }
+                }
+
+                if (allDead)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("모든 몬스터를 처치했습니다! 전투 종료!");
+                    Console.ResetColor();
+
+                    GameManager.BattleController.Battlevictory();
+                    // InitMonsters();
+
+                    Console.WriteLine("계속하려면 아무 키나 눌러주세요");
+                    Console.ReadKey(true);
+                    // Environment.Exit(0);
+
+                    return;
+                }
         }
 
         static void MonsterAttackPhase()
@@ -161,14 +172,14 @@ namespace IPG
 
             while (true)
             {
-                if (monsterIndex >= monsters.Count)
+                if (monsterIndex >= GameManager.ListMonsters.Count)
                 {
                     Console.WriteLine("\n0.다음");
                     //플레이어턴 으로
                     break;
                 }
 
-                var monster = monsters[monsterIndex];
+                var monster = GameManager.ListMonsters[monsterIndex];
                 monsterIndex++;
 
                 if (monster.IsDead)
@@ -181,11 +192,11 @@ namespace IPG
 
                 
                 Console.WriteLine($"\nLv.{monster.Level} {monster.Name}의 공격!");
-                player.Hp -= monster.Atk;
-                Console.WriteLine($"{player.Name} 을(를) 맞췄습니다. [데미지: {monster.Atk}]");
-                Console.WriteLine($"{player.Name} HP: {player.Hp}");
+                GameManager.PlayerController.Hp -= monster.Atk;
+                Console.WriteLine($"{GameManager.PlayerController.Name} 을(를) 맞췄습니다. [데미지: {monster.Atk}]");
+                Console.WriteLine($"{GameManager.PlayerController.Name} HP: {GameManager.PlayerController.Hp}");
 
-                if (player.Hp <= 0)
+                if (GameManager.PlayerController.Hp <= 0)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("\n[플레이어가 사망했습니다...]\n");
@@ -195,7 +206,7 @@ namespace IPG
                     string defeat = Console.ReadLine();
                     if (defeat == "0")
                     {
-                        controller.BattleLose();
+                        GameManager.BattleController.BattleLose();
                         break; // 죽었을때 갈 화면으로
                     }
                 }
--- IPG/IPG/Script/DungeonLobbyController.cs
+++ IPG/IPG/Script/DungeonLobbyController.cs
@@ -1,20 +1,12 @@
 using System;
+using 연습장.Script;
+
 namespace IPG
 {
     internal class DungeonLobbyController
     {
-        int _unlockedFloor = 1;
-        private readonly PlayerController _player;
-        private readonly Battlecontroller _battleController;
-        private VillageController _village;
-
-        public DungeonLobbyController(PlayerController player, Battlecontroller battleController, VillageController village)
-        {
-            _player = player;
-            _battleController = battleController;
-            _village = village;
-        }
-
+        static int _unlockedFloor = 1;
+            
         public void EnterDungeonLobby()
         {
 
@@ -38,18 +30,22 @@ namespace IPG
                 Console.WriteLine("\n원하시는 행동을 입력해주세요.");
                 Console.Write(">> ");
 
+                Random rand = new Random();
+                int index = rand.Next(1, 4);
+                GameManager.MonsterController.RandomMonsterType(index);
+
                 string input = Console.ReadLine();
 
 
                 if (input == "1")
                 {
-                    _player.Status();
+                    GameManager.PlayerController.Status();
                     WaitInput();
                     continue;
                 }
                 else if (input == "0")
                 {
-                    _village.Enter();
+                    GameManager.VillageController.Enter();
                 }
 
                 if (int.TryParse(input, out int selected) && selected >= 2 && selected <= _unlockedFloor + 1)
@@ -68,23 +64,62 @@ namespace IPG
         private void StartBattle(int chosenFloor)
         {
             Console.Clear();
+
+            Console.WriteLine("뚜벅뚜벅 문 앞으로 다가갑니다.");
+            Console.WriteLine();
+            
+            int[,] gameMap = new int[8, 8]
+            {
+                { 0, 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 0, 0, 0, 0, 0, 0},
+                { 0, 0, 1, 1, 1, 1, 0, 0},
+                { 0, 0, 1, 2, 2, 1, 0, 0},
+                { 0, 0, 1, 2, 2, 1, 0, 0},
+                { 0, 0, 1, 1, 1, 1, 0, 0},
+                { 0, 0, 1, 1, 1, 1, 0, 0},
+                { 0, 0, 1, 1, 1, 1, 0, 0}
+            };
+
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    switch (gameMap[y, x])
+                    {
+                        case 0: Console.Write("□ "); break; // 빈 공간
+                        case 1: Console.Write("■ "); break; // 벽
+                        case 2: Console.Write("Ω "); break;
+                        
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
             Console.WriteLine($"{chosenFloor}층 전투를 시작합니다. 행운을 빕니다.\n");
 
             // 클리어 성공했다고 가정하고 다음 층 열기
-            if (_unlockedFloor < chosenFloor + 1)
+            if (  _unlockedFloor < chosenFloor + 1)
             {
                 _unlockedFloor = chosenFloor + 1;
             }
 
             Console.WriteLine("\n계속하려면 아무 키나 누르세요.");
             Console.ReadKey(true);
-            // 전투 시작
 
             // EnterDungeonLobby();
-            _battleController.Battlestart();
+            GameManager.BattleController.Battlestart();
+            bool isVictory = BattleManager.StartBattleAndCheckVictory();
+
+            if (isVictory && _unlockedFloor < chosenFloor + 1)
+            {
+                _unlockedFloor = chosenFloor + 1;
+                Console.WriteLine($"{chosenFloor}층을 클리어했습니다.");
+                Console.WriteLine("다음 층이 열렸습니다.");
+                
+            }
             // 배틀컨트롤러로 넘어가면 초기화되어 1로 다시 시작 되는 문제.
 
-            WaitInput();
+                WaitInput();
         }
 
         private void WaitInput()
