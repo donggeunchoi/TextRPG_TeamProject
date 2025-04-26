87970e52c22c3f14e3f4fe0479ce02dbb9bac0c6
Persona-L <dlaq310@naver.com>
2025-04-27 02:58:28 +0900

    feat: 디버깅 스킵 기능 추가
    디버깅 모드일 때만 마을로 바로 진입합니다.
    
    개편함 ㄹㅇ 신세계임

--- IPG/IPG/Script/Start.cs
+++ IPG/IPG/Script/Start.cs
@@ -11,18 +11,17 @@ namespace IPG
             GameManager.Init();
             GameManager.MonsterController.SaveMonster();
             GameManager.StoreController.SaveItem();
+#if DEBUG // 디버그 모드일 때만 아래 플레이어 필드대로 설정하고 마을 진입
+            GameManager.PlayerController.Name = "디버그맨";
+            GameManager.PlayerController.Job = "전사";
+            GameManager.PlayerController.baseAtk = 12;
+            GameManager.PlayerController.baseDef = 7;
+            GameManager.PlayerController.maxHp = 120;
+            GameManager.PlayerController.currentHp = 120;
+            GameManager.VillageController.Enter();
+#else // 릴리즈 버전에선 정상적으로 메인 타이틀 실행
             MainTitle.Title();
-
-
-            // 마을 (순환구조 남음)
-            // 상점 (완)
-            // 아이템 (완)
-            // 인벤토리 (완)
-            // 배틀컨트롤러 (완)
-            // 배틀 매니저 (완)
-            // 던전 로비 컨트롤러 (완)
-            // 몬스터 컨트롤러 (완)
-            // 인벤토리 (완)
+#endif
         }
     }
 }
