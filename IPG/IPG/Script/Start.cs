1b97775f15e94647c0a34fcc37518e38d5032fe4
donggeunchoi <cdg01063149045@gmail.com>
2025-04-28 10:08:55 +0900

    디버그 모드 해제
    디버스 모드 해제 합니다

--- IPG/IPG/Script/Start.cs
+++ IPG/IPG/Script/Start.cs
@@ -11,17 +11,17 @@ namespace IPG
             GameManager.Init();
             GameManager.MonsterController.SaveMonster();
             GameManager.StoreController.SaveItem();
-            #if DEBUG // 디버그 모드일 때만 아래 플레이어 필드대로 설정하고 마을 진입
-                       GameManager.PlayerController.Name = "디버그맨";
-                       GameManager.PlayerController.Job = "전사";
-                       GameManager.PlayerController.baseAtk = 12;
-                       GameManager.PlayerController.baseDef = 7;
-                       GameManager.PlayerController.maxHp = 120;
-                       GameManager.PlayerController.currentHp = 120;
-                       GameManager.VillageController.Enter();
-            #else // 릴리즈 버전에선 정상적으로 메인 타이틀 실행
-                       MainTitle.Title();
-            #endif
+            // #if DEBUG // 디버그 모드일 때만 아래 플레이어 필드대로 설정하고 마을 진입
+            //            GameManager.PlayerController.Name = "디버그맨";
+            //            GameManager.PlayerController.Job = "전사";
+            //            GameManager.PlayerController.baseAtk = 12;
+            //            GameManager.PlayerController.baseDef = 7;
+            //            GameManager.PlayerController.maxHp = 120;
+            //            GameManager.PlayerController.currentHp = 120;
+            //            GameManager.VillageController.Enter();
+            // #else // 릴리즈 버전에선 정상적으로 메인 타이틀 실행
+            //            MainTitle.Title();
+            // #endif
             MainTitle.Title();
         }
     }
